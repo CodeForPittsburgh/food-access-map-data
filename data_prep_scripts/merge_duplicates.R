0218363a885434fd062a5e515d12135572196155
maxachis <48846180+maxachis@users.noreply.github.com>
2021-01-03 20:09:54 -0500

    Merge "Backup 2020 01 03" with "Master" (#66)
    * Revert "Update merge_duplicates.R"
    
    This reverts commit cc6e818d831b0c07bd0ed515e69d8e5ad9f005b3.
    
    * Revert "Update merge_duplicates.R"
    
    This reverts commit 396dcc0dcbfb64802adb690ab52e3f0edd66025f.
    
    * Revert "Update merge_duplicates.R"
    
    This reverts commit 17120c05cd463c4ec4f84dea100f073ae4cdd9e4.
    
    * Revert "Update pipfile and add test_merge_duplicates"
    
    This reverts commit 2f1fa55a192a4c7e648f5dcf934dc262e1c5e55b.

--- data_prep_scripts/merge_duplicates.R
+++ data_prep_scripts/merge_duplicates.R
@@ -3,32 +3,12 @@
 # This script merges duplicate rows -- identified by their duplicate group id's -- together
 #  and reconciles disparities in key fields by prioritizing fields based on their source data sets
 # Outputs a merged dataset 
-#install.packages("devtools")
-#devtools::install_github("tidyverse/tidyverse")
 
 library(tidyverse)
-library(utils)
 input_filename <- "intermediate_data/merge_duplicates_input_test.csv"
 output_filename <- "intermediate_data/merge_duplicates_output.csv"
 
-#This function and the below two commands set the script as the current working directory
-thisFile <- function() {
-  cmdArgs <- commandArgs(trailingOnly = FALSE)
-  needle <- "--file="
-  match <- grep(needle, cmdArgs)
-  if (length(match) > 0) {
-    # Rscript
-    return(normalizePath(sub(needle, "", cmdArgs[match])))
-  } else {
-    # 'source'd via R console
-    return(normalizePath(sys.frames()[[1]]$ofile))
-  }
-}
-script.dir <- dirname(thisFile())
-setwd(script.dir)
-
-
-sfp <- read_csv("source_field_prioritization_sample_data.csv")
+sfp <- read_csv("source_field_prioritization.csv")
 md <- read_csv(input_filename)
 md$merged_record = '0'
 
