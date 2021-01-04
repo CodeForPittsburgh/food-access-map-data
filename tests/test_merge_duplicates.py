cc6e818d831b0c07bd0ed515e69d8e5ad9f005b3
Max Chis <maxachis@gmail.com>
2021-01-03 19:34:19 -0500

    Update merge_duplicates.R

--- tests/test_merge_duplicates.py
+++ tests/test_merge_duplicates.py
@@ -20,13 +20,12 @@ utils.chooseCRANmirror(ind=1) # select the first mirror in the list
 utils.chooseCRANmirror(ind=1) # select the first mirror in the list
 
 # R package names (can be more than one)
-packnames = ('tidyverse', '')
+packnames = ('tidyverse', 'ggplot2')
 
 # R vector of strings
 from rpy2.robjects.vectors import StrVector
 
 # Selectively install what needs to be install.
-# We are fancy, just because we can.
 names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
 if len(names_to_install) > 0:
     utils.install_packages(StrVector(names_to_install))
