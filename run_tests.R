525f151da7c416afaba0ac801ac7db103189b0d4
Max Chis <maxachis@gmail.com>
2021-02-13 11:16:13 -0500

    Modify run_tests.R to take API key as parameter from github actions

--- run_tests.R
+++ run_tests.R
@@ -1,3 +1,13 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("Mapbox API Key must be called as first argument", call.=FALSE)
+} else if (length(args)==1) {
+  API_KEY <- args[1]
+}
+
 library("testthat")
 
 testthat::test_dir("tests")
