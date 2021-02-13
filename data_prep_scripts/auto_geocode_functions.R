cd6a60956cbea858a11ba121cbbe5219be80a1c1
Max Chis <maxachis@gmail.com>
2021-02-13 09:53:45 -0500

    Add new geocode function and geocode tests

--- data_prep_scripts/auto_geocode_functions.R
+++ data_prep_scripts/auto_geocode_functions.R
@@ -1,11 +1,14 @@
 library(stringr)
 library(purrr)
 library(dplyr)
-library(tidygeocoder)
+library(httr)
+
+#POSSIBLY REMOVE THESE LINES IF WE HAVE TO FOR AUTOMATION
+#library(rstudioapi)
+#API_KEY <- askForPassword(prompt = "Enter Mapbox API Key")
+#--END PROPOSED REMOVES
 
 run_geocode <- function(df) {
-  Sys.setenv(GEOCODIO_API_KEY="64735d54370c29c4458c32d44306323007a0d60")
-  
   # STEP 1 
   df_filter <- df %>% 
     filter(is.na(latitude) | is.na(longitude) | latitude==0 | longitude==0) 
@@ -13,10 +16,15 @@ run_geocode <- function(df) {
   # STEP 2
   df_transmute <- df_filter %>% transmute(id = id, address = str_c(address, city, state, zip_code, sep = ", "))
   
-  # STEP 3
-  df_geocode <- geocode(df_transmute, address, geo="osm", cascade_order=c("osm", "geocodio"))
+  # Step 3
+  df_geocode <- df_transmute %>% mutate(geometry=map(address, geocode_single))
   
-  # STEP 4
+  # Step 4
+  df_geocode <- df_geocode %>% mutate(long = unlist(map(geometry, get_longitude)), lat=unlist(map(geometry, get_latitude)))
+  df_geocode <- df_geocode %>% select(-geometry)
+  
+  
+  # STEP 5
   df <- df %>% left_join(df_geocode %>% select(id, long, lat), by = "id")
   df <- df %>% mutate(longitude = ifelse(is.na(longitude), long, longitude),
                       latitude = ifelse(is.na(latitude), lat, latitude),
@@ -24,4 +32,21 @@ run_geocode <- function(df) {
   df <- df %>% select(-lat, -long) 
   
   return(df)
+}
+
+geocode_single <- function(search) {
+  url <- URLencode(paste("https://api.mapbox.com/geocoding/v5/mapbox.places/", search,".json?access_token=", API_KEY, sep = ""))
+  r <- GET(url)
+  c <- content(r, as="parsed", type="application/json")
+  features <- c[[3]]
+  geometry <- paste(features[[1]]$geometry[2][[1]], collapse=";")
+  return(geometry)
+}
+
+get_latitude <- function(geometry) {
+  return(as.numeric(unlist(strsplit(geometry, ";")[1][[1]][[2]])))
+}
+
+get_longitude <- function(geometry) {
+  return(as.numeric(unlist(strsplit(geometry, ";")[1][[1]][[1]])))
 }
--- tests/test-geocode.R
+++ tests/test-geocode.R
@@ -1,17 +1,20 @@
 #Tests for merge-duplicates
 library("readr")
 
-test_that("Merge_duplicates creates hybrid id's for merged rows",  {
-  df <- read_csv("test-data/test_geocode_one_row_input.csv")
+test_that("Geocode returns expected coordinates for single row",  {
   source('../data_prep_scripts/auto_geocode_functions.R')
-  result <- run_geocode(df)
+  input <- read_csv("test-data/test_geocode_one_row_input.csv")
+  exp_output <- read_csv("test-data/test_geocode_one_row_exp_output.csv")  
+  output <- run_geocode(input)
+  expect_equal(round(output$longitude, digits=2), round(exp_output$longitude, digits=2))
+  expect_equal(round(output$latitude, digits=2), round(exp_output$latitude, digits=2))
 })
 
-test_that("Merge_duplicates marks merge_rows with a '1' in 'merged_record' column", {
-  df <- read_csv("test-data/test_merge_two_rows_no_conflicts_input.csv")
-  sfp <- read_csv("../data_prep_scripts/source_field_prioritization_sample_data.csv")
-  source('../data_prep_scripts/merge_duplicates_functions.R')
-  result <- merge_all_duplicates_in_dataframe(df, sfp)
-  final_merged_record <- result$merged_record
-  expect_equal(final_merged_record, "1")
+test_that("Geocode returns expected coordinates for multiple rows", {
+  input <- read_csv("test-data/test_geocode_three_rows_input.csv")
+  exp_output <- read_csv("test-data/test_geocode_three_rows_exp_output.csv")
+  source('../data_prep_scripts/auto_geocode_functions.R')
+  output <- run_geocode(input)
+  expect_equal(round(output$longitude, digits=2), round(exp_output$longitude, digits=2))
+  expect_equal(round(output$latitude, digits=2), round(exp_output$latitude, digits=2))
 })
