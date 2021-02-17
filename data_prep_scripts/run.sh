44512e2caeea3bf877d6ff7a7140da617019688b
Catalina Moreno <cmoreno@andrew.cmu.edu>
2021-02-16 19:15:44 -0500

    exe files2

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Rscript data_prep_scripts/auto_agg_clean_data.R food-data/Cleaned_data_files/ | Rscript data_prep_scripts/auto_text_process_name.R | Rscript data_prep_scripts/auto_geocode_wrapper.R ${{ secrets.MAPBOX_API_KEY }}| Rscript data_prep_scripts/auto_write_to_csv.R
+Rscript data_prep_scripts/auto_agg_clean_data.R food-data/Cleaned_data_files/ | Rscript data_prep_scripts/auto_text_process_name.R | Rscript data_prep_scripts/auto_geocode_wrapper.R $mapbox_key | Rscript data_prep_scripts/auto_write_to_csv.R
 
