ea12ccad3537f362e3ced4813b273e1c586a4ccb
maxachis <48846180+maxachis@users.noreply.github.com>
2021-02-22 19:44:46 -0500

    Add workflow.yaml and update geocoder functions (#80)
    * auto run files
    
    * exe files
    
    * exe files2
    
    * remove tidygeocoder
    
    * source path
    
    * fix input name
    
    * workflow yaml with cfp username
    
    * Update auto_geocode_functions.R
    
    geocode_single method was erroneously retrieving the wrong data on some searches, causing the geocoding process to break. This has been corrected.
    
    * newly updated merged datasets
    
    Co-authored-by: Catalina Moreno <cmoreno@andrew.cmu.edu>
    Co-authored-by: CodeForPittsburgh <CodeForPittsburgh@users.noreply.github.com>

--- data_prep_scripts/auto_geocode_wrapper.R
+++ data_prep_scripts/auto_geocode_wrapper.R
@@ -14,15 +14,24 @@
 ## load pkg
 
 suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(tidygeocoder))
+# suppressPackageStartupMessages(library(tidygeocoder))
 
-source("auto_geocode_functions.R")
+source("data_prep_scripts/auto_geocode_functions.R")
+
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("Mapbox API Key must be called as first argument", call.=FALSE)
+} else if (length(args)==1) {
+  API_KEY <- args[1]
+}
 
 ## read in stdin() from previous step
 input <- file('stdin', 'r')
 
 ## read.table() bc stdin input
-all_datasets <- read.table(input_dat)
+all_datasets <- read.table(input)
 
 ## run function with datatable input
 all_datasets <- run_geocode(all_datasets)
