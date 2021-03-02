49744bf76d4fb257f8e1aedc26b8af6b8b66aa97
Matt Sandler <mishugana@gmail.com>
2021-03-02 18:31:53 -0500

    Added the hack for git action workflow

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -2,3 +2,6 @@
 
 Rscript data_prep_scripts/auto_agg_clean_data.R food-data/Cleaned_data_files/ | Rscript data_prep_scripts/auto_text_process_name.R | Rscript data_prep_scripts/auto_geocode_wrapper.R $mapbox_key | Rscript data_prep_scripts/auto_write_to_csv.R
 
+CURRENTDATE=`date +"%Y-%m-%d %T"`
+echo Updated at: ${CURRENTDATE} > date.txt
+
