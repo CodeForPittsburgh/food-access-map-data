e27a2826a457e0d0cf37c45e2373fd1721051e97
maxachis <48846180+maxachis@users.noreply.github.com>
2021-03-14 20:43:58 -0400

    Add unfinished id_duplicate scripts and associated files (#87)
    * Modify run.sh and migrate scripts from iPython notebooks to Python scripts
    
    * Add unfinished id_duplicates, wrapper, function, and test files
    
    Matt updated this version and added new modules. Before we integrate it fully, I'd like to add/run tests on various edge cases to ensure that it catches them. Considering we want to identify duplicates and identified duplicates will be merged/deleted, I want to be extra certain it functions alright. I figure it'll leave us with less headaches if we ensure ahead of time that we're not getting any false positives/negatives.
    
    Developing and running those tests will take additional time. Additionally, I can't run the script on my Windows OS machine--one of the modules can only be installed reliably on Linux and Mac OS, which may require discussion as to whether we want a module that doesn't work on Windows OS -- this won't stop our Github automation, which runs on a Linux OS. But it does slow down anyone who wants to test or develop it on a Windows Machine. Let's make sure we're fine with that.

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-Rscript data_prep_scripts/auto_agg_clean_data.R food-data/Cleaned_data_files/ | Rscript data_prep_scripts/auto_text_process_name.R | Rscript data_prep_scripts/auto_geocode_wrapper.R $mapbox_key | Rscript data_prep_scripts/auto_write_to_csv.R
+#NOTE TO REMOVE LATER: Try adding breaking down into intermediate text files for better debugging.
+
+Rscript data_prep_scripts/auto_agg_clean_data.R food-data/Cleaned_data_files/ | \
+	Rscript data_prep_scripts/auto_text_process_name.R | \
+	Rscript data_prep_scripts/auto_geocode_wrapper.R $mapbox_key | \
+	# Py data_prep_scripts/auto_clean_addresses_wrapper.py | \
+	# Py data_prep_scripts/auto_id_duplicates_wrapper.py | \
+	# Rscript data_prep_scripts/auto_merge_duplicates_wrapper.py | \
+	Rscript data_prep_scripts/auto_write_to_csv.R
 
 CURRENTDATE=`date +"%Y-%m-%d %T"`
 echo Updated at: ${CURRENTDATE} > date.txt
---
+++ tests/test_id_duplicates.py
@@ -0,0 +1,15 @@
+#test_dedupe.py
+
+import os.path
+from os import path
+
+def test_id_duplicates_2_rows():
+	return True
+
+#NAME: test_id_duplicates_same_street_diff_num
+#Test to see if duplicates are detected between two streets, both with only one number different in the number
+
+#NAME: test_id_duplicates_same_street_diff_town
+#Test to see if duplicates are detected between two streets, both with only the town different
+
+#NAME: test_id_duplicates_
