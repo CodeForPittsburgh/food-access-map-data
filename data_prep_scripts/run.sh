1793d3cac4eeb9e6b050693b9ed614e7edd5eefc
maxachis <48846180+maxachis@users.noreply.github.com>
2021-04-13 18:51:45 -0400

    Update run.sh

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -14,7 +14,8 @@ Rscript data_prep_scripts/auto_agg_clean_data.R food-data/Cleaned_data_files/ | 
 	python data_prep_scripts/auto_clean_addresses_wrapper.py | \
 	python data_prep_scripts/auto_id_duplicates_wrapper.py | \
 	Rscript data_prep_scripts/auto_merge_duplicates_wrapper.R "data_prep_scripts/source_field_prioritization_sample_data.csv" > food-data/processed-datasets/merged_datasets.csv
-	cp merged_datasets.csv $new_fileName
+
+cp merged_datasets.csv $new_fileName
 
 CURRENTDATE=`date +"%Y-%m-%d %T"`
 echo Updated at: ${CURRENTDATE} > date.txt
