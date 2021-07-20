a8c110278fabe1874841227e25504d659f011e7b
Max Chis <maxachis@gmail.com>
2021-07-19 20:46:43 -0400

    Modify run.sh and begin work on data_integrity_wrapper file

---
+++ data_prep_scripts/auto_check_source_data_integrity_wrapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+
+args = sys.argv
+if len(arg) != 2:
+	#Stop and throw error, because you need an argument for this
+	print("ERROR: Insufficient number of arguments for script")
+	return false
+
+
+
+
+#Go through all of the files and check that ...
+	# None of the datasets to be merged have 0 rows
+	# They have the columns required as stipulated by the schema
+
+#Send a signal that the next script would read?
+
+# Close the files
+
+
