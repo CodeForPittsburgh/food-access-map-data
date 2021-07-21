2fef1500b9158f49f20ba287d01051de615c1700
Oscar Syu <oscar.syu@berkeley.edu>
2021-07-21 18:21:34 -0400

    Update source_py_scripts.py

--- data_prep_scripts/prep_source_scripts/source_py_scripts.py
+++ data_prep_scripts/prep_source_scripts/source_py_scripts.py
@@ -6,7 +6,7 @@
 #Haikus are easy
 from tqdm import tqdm
 
-files = ["prep_Greater_Pittsburgh_Community_Food_Bank.py",
+files = ["prep_FMNP.py", "prep_Greater_Pittsburgh_Community_Food_Bank.py",
         "prep_Just_Harvest.py", "prep_Pittsburgh_Food_Policy_Council.py", "prep_The_Food_Trust.py",
         "prep_USDA_Food_And_Nutrition.py"]
 for filename in tqdm(files):
