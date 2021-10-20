71fe70b148b222b97efd493e75dcc4712e6e35f5
maxachis <48846180+maxachis@users.noreply.github.com>
2021-10-20 18:52:17 -0400

    Update run.sh

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -7,6 +7,13 @@ echo "Current Time : $current_time"
 new_fileName=$file_name.$current_time.csv
 echo "New FileName: " "$new_fileName"
 
+#Get Wic Results from sort-of-API
+wget -q -O - --method POST \
+--header 'Accept: application/json, text/javascript, /; q=0.01' \
+--header 'Content-Type: application/json; charset=UTF-8' \
+--body-data '{"county":"2","zip":"","miles":"5"}' \
+'https://www.pawic.com/FindWICStores.aspx/FindStores' | jq -r '.d' | jq > wicresults.json
+
 #Prepare source scripts -- scripts that obtain the requisite data from APIs, Googlesheets, and Repository CSVs.
 python data_prep_scripts/prep_source_scripts/source_py_scripts.py
 Rscript data_prep_scripts/prep_source_scripts/source_r_scripts.R
