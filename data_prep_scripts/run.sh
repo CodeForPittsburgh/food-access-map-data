51130edeb4b6d6f5e8ba30ea68dae89c1b0e7eb7
maxachis <48846180+maxachis@users.noreply.github.com>
2021-10-20 19:31:36 -0400

    Update run.sh

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -12,10 +12,8 @@ wget -q -O - --method POST \
 --header 'Accept: application/json, text/javascript, /; q=0.01' \
 --header 'Content-Type: application/json; charset=UTF-8' \
 --body-data '{"county":"2","zip":"","miles":"5"}' \
-'https://www.pawic.com/FindWICStores.aspx/FindStores' | jq -r '.d' | jq > food-data/new-datasets/thiccresults.json
-
-tail food-data/new-datasets/wicresults.json
-echo 'TESTING YO'
+'https://www.pawic.com/FindWICStores.aspx/FindStores' | jq -r '.d' | jq > food-data/new-datasets/wicresults.json
+cat wicresults.json  | jq ‘.timestamp |= "$current_time"’
 
 #Prepare source scripts -- scripts that obtain the requisite data from APIs, Googlesheets, and Repository CSVs.
 python data_prep_scripts/prep_source_scripts/source_py_scripts.py
