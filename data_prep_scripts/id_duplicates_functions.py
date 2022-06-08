93c303e3999ade421c18c037b519e43885b7c65d
maxachis <maxachis@gmail.com>
2022-06-08 12:42:53 -0400

    Updated training.json by adding "type" field to compare, and made changes to id_duplicates_functions.py to accomodate this.

--- data_prep_scripts/id_duplicates_functions.py
+++ data_prep_scripts/id_duplicates_functions.py
@@ -4,8 +4,12 @@ def id_duplicates(data):
     return
 
 def get_relevant_data(df):
+    #This takes all empty strings and replaces them with a single space -- this is a workaround because the deduper cannot compare empty strings.
+    df = df.replace(r'^\s*$', " ", regex=True)
+
     rd = df[['name', 'address']].astype(str)
     rd['LatLong'] = list(zip(df.latitude, df.longitude))
+    rd['type'] = df['type'].astype(str)
     data = rd.to_dict(orient='index')
     return data
 
@@ -28,7 +32,8 @@ def initialize_deduper(data):
     # initialize from a defined set of fields
     variables = [{'field' : 'name', 'type': 'String'},
                  {'field' : 'address', 'type': 'String'},
-                 {'field' : 'LatLong', 'type': 'LatLong'}
+                 {'field' : 'LatLong', 'type': 'LatLong'},
+                 {'field' : 'type', 'type': 'String'}
                 ]
     deduper = dedupe.Dedupe(variables)
     
